789711c84be99fe69c01c31ab6ab4d9a1ed8e364
Denis da Rosa Ferreira <115656630+DenisFerreira90@users.noreply.github.com>
2023-11-05 20:37:28 -0300

    att

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const login_router = require ('./router/login_router')
-const livros_router = require ('./router/livros.router');
+const livros_router = require ('./router/livros_router');
 const acesso_middleware = require ('./middleware/acesso_middleware')
 
 const app = express()
@@ -9,12 +9,19 @@ const port = 3001
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('metodo'+ req.method);
+  console.log('Metodo '+ req.method);
   next();
 })
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+});
 
-app.use('./')
+app.use('/router', login_router);
+
+app.use(acesso_middleware.verificaAcesso);
+
+app.use('./router', livros_router);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
